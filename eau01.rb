551aed86e13663e1115a4c0768528c3a5a4827be
Akeem57 <lyrlandais@hotmail.com>
2023-07-24 19:35:20 +0200

    debut eau01

---
+++ eau01.rb
@@ -0,0 +1,12 @@
+=begin
+Créez un programme qui affiche toutes les différentes combinaisons de deux nombre entre 
+00 et 99 dans l’ordre croissant.
+
+
+Exemples d’utilisation :
+$> python exo.py
+00 01, 00 02, 00 03, 00 04, ... , 00 99, 01 02, ... , 97 99, 98 99
+$>
+
+=end
+
