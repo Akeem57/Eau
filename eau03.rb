c971ade0c62f62a5eafe80e77ddd303eeb87bf11
Akeem57 <lyrlandais@hotmail.com>
2023-07-27 19:22:13 +0200

    27/07 19:22

---
+++ eau03.rb
@@ -0,0 +1,20 @@
+=begin
+Créez un programme qui affiche le N-ème élément de la célèbre suite de Fibonacci. (0, 1, 1, 2) étant le début de la suite et le premier élément étant à l’index 0.
+
+Exemples d’utilisation :
+$> python exo.py 3
+2
+$>
+
+Afficher -1 si le paramètre est négatif ou mauvais.
+=end
+
+#1. Fonctions
+ 
+#2. Erreur
+
+#3. Parse
+
+#4. Résolution
+
+#5. Affichage
