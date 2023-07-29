7fece0df80ca013ef511b30f49401e0f89a03a4d
Akeem57 <lyrlandais@hotmail.com>
2023-07-29 09:24:41 +0200

    29/07 09:24

---
+++ eau04.rb
@@ -0,0 +1,20 @@
+=begin
+#Créez un programme qui affiche le premier nombre premier supérieur au nombre donné en argument.
+
+Exemples d’utilisation :
+$> python exo.py 14
+17
+$>
+
+Afficher -1 si le paramètre est négatif ou mauvais.
+=end
+
+#1. Fonctions
+ 
+#2. Erreur
+
+#3. Parse
+
+#4. Résolution
+
+#5. Affichage
