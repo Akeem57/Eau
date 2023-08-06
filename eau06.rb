3a20e2d8f216c307753aad7234442b1b35ecfc3b
Akeem57 <lyrlandais@hotmail.com>
2023-08-06 13:30:13 +0200

    fin eau06

--- eau06.rb
+++ eau06.rb
@@ -10,4 +10,24 @@ error
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 =end
+def alternate_case_oneletter_ontwo(arg)
+    arg = ARGV[0]
+    new_arg = arg.each_char.with_index.map do |char, index| 
+        if index % 2 == 0
+            char.upcase == char ? char.downcase : char.upcase
+        else
+            char
+        end
+    end
+    return new_arg.join
+end
 
+if ARGV.length != 1 || ARGV[0].to_i.to_s == ARGV[0] 
+    puts "error"
+    exit
+end
+
+input = ARGV[0]
+result = alternate_case_oneletter_ontwo(input)
+
+puts result
