ca009d1a062387d6f31cec36b1f9e996e659d469
Akeem57 <lyrlandais@hotmail.com>
2023-08-28 18:38:11 +0200

    fin 28/08

--- eau08.rb
+++ eau08.rb
@@ -13,3 +13,20 @@ false
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 =end
+
+def contains_only_digits?(input)
+   input.match?(/\A\d+\z/)
+end
+
+if ARGV.length != 1
+    puts "error"
+    exit
+end
+
+argument = ARGV[0]
+  
+if contains_only_digits?(argument)
+    puts "true"
+else
+    puts "false"
+end
