b35b94d4e9d7c556360cbc9a4d0c47803468526c
Akeem57 <lyrlandais@hotmail.com>
2023-09-03 08:11:33 +0200

    Entre min et max

--- eau09.rb
+++ eau09.rb
@@ -2,8 +2,8 @@
 Créez un programme qui affiche toutes les valeurs comprises entre deux nombres dans l’ordre croissant. Min inclus, max exclus.
 
 Exemples d’utilisation :
-$> python exo.py 20 25
-20 21 22 23 24
+$> python exo.py 20 
+20 21 22 23 24 
 
 $> python exo.py 25 20
 20 21 22 23 24
