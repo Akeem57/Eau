025e5f83893898df321db25ea7d3a07ef35790f3
Akeem57 <lyrlandais@hotmail.com>
2023-09-03 08:43:57 +0200

    Fin Chapitre EAU

--- eau15.rb
+++ eau15.rb
@@ -8,3 +8,10 @@ $>
 
 Note : [] est à remplacer par un adjectif de votre choix (facile ?)
 =end
+
+if ARGV.length != 1 || ARGV.any? {|arg| !arg.match?(/\A[a-zA-Z]+\z/)}
+    puts "Veuillez entrer un adjectif"
+    exit
+end
+
+puts "J'ai terminé l'Épreuve de l’Eau et c’était #{ARGV[0]}"
