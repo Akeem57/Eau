a979b027ab9d49890b1d485cc2fc7876afe53468
Akeem57 <lyrlandais@hotmail.com>
2023-09-03 08:06:48 +0200

    Tri par sélection

--- eau13.rb
+++ eau13.rb
@@ -18,3 +18,27 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 Wikipedia vous présentera une belle description de cet algorithme de tri.
 =end
+
+if ARGV.length < 2 || ARGV.any? {|args| !args.match?(/\A-?\d+\z/)}
+    puts "error"
+    exit
+end
+
+def my_selection_sort(array)
+	n = array.length
+
+	(n - 1).times do |i|
+	  	min_index = i
+	  	(i + 1...n).each do |j|
+			min_index = j if array[j] < array[min_index]
+	  	end
+	  	array[i], array[min_index] = array[min_index], array[i] if i != min_index
+	end
+	return array
+end
+
+numbers = ARGV.map(&:to_i)
+
+sorted_numbers = my_selection_sort(numbers)
+
+puts "Liste triée : #{sorted_numbers.join(' ')}"
