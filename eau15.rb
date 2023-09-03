c90e277e1168343b349ea44a47c8af883720fb16
Akeem57 <lyrlandais@hotmail.com>
2023-09-03 07:30:20 +0200

    debut

---
+++ eau15.rb
@@ -0,0 +1,10 @@
+=begin
+Créez un programme qui célèbre votre victoire.
+
+Exemples d’utilisation :
+$> ruby exo.rb
+J’ai terminé l’Épreuve de l’Eau et c’était [].
+$>
+
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+=end
